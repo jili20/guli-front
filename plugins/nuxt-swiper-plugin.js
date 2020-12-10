00106a3dbda9a67ab05ce9c01140c297a12c295f
Bing <bing_yu2001@163.com>
2020-12-10 19:49:15 +0800

    手机短信 注册 登录

--- plugins/nuxt-swiper-plugin.js
+++ plugins/nuxt-swiper-plugin.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr'
-
-Vue.use(VueAwesomeSwiper)
+import VueAwesomeSwiper from 'vue-awesome-swiper/dist/ssr' // 轮播图插件
+import VueQriously from 'vue-qriously' // 微信支付二维码插件
+import ElementUI from 'element-ui' // element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'// element-ui的css
+Vue.use(ElementUI) // 使用elementUI
+Vue.use(VueQriously) // 使用微信支付二维码插件
+Vue.use(VueAwesomeSwiper) // 使用轮播图插件
